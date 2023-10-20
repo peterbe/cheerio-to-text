afad4d99f6cf5800bc3a9c9927c624a92bfbabf9
Peter Bengtsson <mail@peterbe.com>
2023-10-20 12:47:07 -0400

    hacking

--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,5 @@
 import { join } from "path"
-import { mkdirSync, renameSync } from "fs"
+import { copyFileSync, mkdirSync, renameSync } from "fs"
 
 const DIST_DIR = "./lib"
 
@@ -17,27 +17,32 @@ await Bun.build({
 //   format: "cjs",
 // })
 
-// await retroFix()
+await retroFix()
 
 async function retroFix() {
   // Got this from
   // https://github.com/oven-sh/bun/issues/5707#issuecomment-1730511178
   const IMPORT_META_REQUIRE_POLYFILL = `import { createRequire as createImportMetaRequire } from "module"; import.meta.require ||= (id) => createImportMetaRequire(import.meta.url)(id);`
-  const ESM_DIR = join(DIST_DIR, "esm")
-  mkdirSync(ESM_DIR, { recursive: true })
-  renameSync("lib/index.js", "lib/esm/index.js")
-  const ESM_FILE = join(ESM_DIR, "index.js")
-  const outputFile = Bun.file(ESM_FILE)
-  const output = await outputFile.text()
-  const newLines: string[] = []
-  for (const line of output.split("\n")) {
-    newLines.push(line)
-    if (line.startsWith("#!")) {
-      newLines.push(IMPORT_META_REQUIRE_POLYFILL)
-    }
-  }
-  const newOutput = newLines.join("\n")
-  await Bun.write(ESM_FILE, newOutput)
+  // renameSync("lib/index.js", "lib/esm/index.js")
+  const CJS_FILE = join(DIST_DIR, "index.js")
+  copyFileSync(join(DIST_DIR, "esm", "index.js"), CJS_FILE)
+
+  // const outputFile = Bun.file(CJS_FILE)
+  // const output = await outputFile.text()
+  // const newLines: string[] = []
+  // newLines.push(IMPORT_META_REQUIRE_POLYFILL)
+  // for (const line of output.split("\n")) {
+  //   newLines.push(line)
+  //   // if (line.startsWith("#!")) {
+  //   //   console.log("WRITING")
+
+  //   // }
+  // }
+  // const newOutput = newLines.join("\n")
+
+  // // console.log(newOutput.slice(0, 2000))
+
+  // await Bun.write(CJS_FILE, newOutput)
 
   await Bun.write(join(DIST_DIR, "esm", "package.json"), '{"type":"module"}')
   // await Bun.write(DIST_FILE, newOutput)
